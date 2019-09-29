f8f280cde8ac31349fa322aaaedd80d0e6280116
Nick Ramsay <NickRamsay1989@gmail.com>
2019-09-29 16:31:46 +1000

    movie-this now functional

--- liri.js
+++ liri.js
@@ -42,9 +42,54 @@ switch (process.argv[2]) {
                 spotifyData();
         }
         break;
+    case "movie-this":
+        command = process.argv[2];
+        switch (process.argv[3]) {
+            case undefined:
+                search = "Mr. Nobody";
+                movies();
+                break;
+            default:
+                search = process.argv[3];
+                movies();
+        }
+        break;
     default:
         console.log("Sorry, invalid command. Try again.")
         break;
+}
+
+function movies() {
+    axios({
+        method: 'get',
+        url: 'http://www.omdbapi.com/?apikey=trilogy&t=' + search
+    }).then(function (response) {
+        movies = response.data;
+        renderMovies();
+    })
+}
+
+function renderMovies() {
+    var separator = "--------------------------------------------";
+    var ratings = movies.Ratings;
+    var rtRating = "Not Rated";
+    for (i = 0; i < ratings.length; i++) {
+        switch (ratings[i].Source) {
+            case "Rotten Tomatoes":
+                rtRating = ratings[i].Value;
+                break;
+        }
+    }
+    console.log(separator);
+    console.log("Title: " + movies.Title);
+    console.log("Year: " + movies.Year);
+    console.log("IMDB Rating: " + movies.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + rtRating);
+    console.log("Country of Prodution: " + movies.Country);
+    console.log("Language: " + movies.Language);
+    console.log("Plot: " + movies.Plot);
+    console.log("Actors: " + movies.Actors);
+    console.log(separator);
 }
 
 function spotifyData() {
@@ -61,6 +106,7 @@ function spotifyData() {
 function renderSpotifyData() {
     for (i = 0; i < spotifyData.length; i++) {
         var separator = "--------------------------------------------";
+        console.log(separator);
         console.log("Artist(s): " + spotifyData[i].artists[0].name);
         console.log("Song: " + spotifyData[i].name);
         console.log("Album: " + spotifyData[i].album.name);
@@ -68,7 +114,8 @@ function renderSpotifyData() {
         console.log(separator);
     }
 }
-//NOTE: This should be enhanced to report all artist names if multiple artists exist in array
+//NOTE 1: Update so if search parameter is missing, it will specifically pick "The Sign" by Ace of Base.
+//NOTE 2: This should be enhanced to report all artist names if multiple artists exist in array
 function concert() {
     axios({
         method: 'get',
@@ -83,6 +130,7 @@ function renderConcerts() {
     for (i = 0; i < concerts.length; i++) {
         var concertDate = new Date(concerts[i].datetime);
         var separator = "--------------------------------------------";
+        console.log(separator);
         console.log("Venue: " + concerts[i].venue.name);
         console.log("Location: " + concerts[i].venue.city + ", " + concerts[i].venue.country);
         console.log("Concert Date: " + moment(concertDate).format('MM/DD/YYYY'));
