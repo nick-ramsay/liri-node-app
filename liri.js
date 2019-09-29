41bb850027fd25a6bf93e72fc1281be23fd5dd34
Nick Ramsay <NickRamsay1989@gmail.com>
2019-09-29 15:16:26 +1000

    spotify-this-song now functioning

--- liri.js
+++ liri.js
@@ -4,32 +4,71 @@ var axios = require('axios');
 
 var moment = require('moment');
 
+var Spotify = require('node-spotify-api');
+
 var keys = require("./keys.js");
 
-//var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var command;
 //var search = process.argv[3];
-var search = "metallica";
+var search = process.argv[3];
 
 var concerts;
+var spotifyData;
+
 
 switch (process.argv[2]) {
-    case "concert-this" || "spotify-this-song" || "movie-this" || "do-what-it-says":
+    case "concert-this":
         command = process.argv[2];
-        switch (command) {
-            case "concert-this":
-                concert();
+        switch (process.argv[3]) {
+            case undefined:
+                console.log("No search criteria provided. Please try again.");
                 break;
             default:
-                ""
+                search = process.argv[3];
+                concert();
+        }
+        break;
+    case "spotify-this-song":
+        command = process.argv[2];
+        switch (process.argv[3]) {
+            case undefined:
+                search = "The Sign";
+                spotifyData();
+                break;
+            default:
+                search = process.argv[3];
+                spotifyData();
         }
         break;
     default:
-        console.log("Sorry, invalid command. Try again.");
+        console.log("Sorry, invalid command. Try again.")
+        break;
 }
 
+function spotifyData() {
+    spotify.search({ type: 'track', query: search }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        spotifyData = data.tracks.items;
+        console.log(spotifyData[0].artists);
+        renderSpotifyData();
+    });
+}
 
+function renderSpotifyData() {
+    for (i = 0; i < spotifyData.length; i++) {
+        var separator = "--------------------------------------------";
+        console.log("Artist(s): " + spotifyData[i].artists[0].name);
+        console.log("Song: " + spotifyData[i].name);
+        console.log("Album: " + spotifyData[i].album.name);
+        console.log("Preview URL: " + spotifyData[i].preview_url);
+        console.log(separator);
+    }
+}
+//NOTE: This should be enhanced to report all artist names if multiple artists exist in array
 function concert() {
     axios({
         method: 'get',
